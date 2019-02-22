109cc911d7241198ca7f67160525c6d03f9c5b57
Arwed Mett <arwed.mett@googlemail.com>
2019-02-22 07:56:33 +0100

    fixed flowtype errors and introduced checkers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 //@flow
 
-export type { Description, Type, Property, Children } from "./types"
-export { is_object, is_attribute } from "./checker"
+export type {
+	Description,
+	Type,
+	Property,
+	Children
+} from "./types"
+
+export {
+	is_object,
+	is_string,
+	is_number,
+	is_array,
+	is_null,
+	is_boolean,
+	is_attribute
+} from "./checker"
+
 export { default as default } from "./sanitize"
 export { object, array, number, string, boolean, NULL } from "./definition"
+
 export {
 	get_string,
 	get_number,
--- src/types/index.js
+++ src/types/index.js
@@ -1,13 +1,6 @@
 //@flow
 
-import type { Type as _Type } from "./type"
-export type Type = _Type
-
-import type { Property as _Property } from "./property"
-export type Property = _Property
-
-import type { Children as _Children } from "./children"
-export type Children = _Children
-
-import type { Description as _Description } from "./description"
-export type Description = _Description
+export type { Type } from "./type"
+export type { Property } from "./property"
+export type { Children } from "./children"
+export type { Description } from "./description"
